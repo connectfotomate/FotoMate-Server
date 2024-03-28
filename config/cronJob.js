612ea8bb6689bd4836a21d5d1d475b6c77266bcd
sandeepnp05 <npsandeep05@gmail.com>
2024-03-28 19:24:00 +0530

    feat: node crone job

--- config/cronJob.js
+++ config/cronJob.js
@@ -1,30 +1,31 @@
+import cron from 'node-cron';
+import https from 'https';
 
-import cron from 'node-cron'
-import https from 'https'
-
-const job = cron.schedule('*/14 * * * *', () => {
+function pingServer() {
     console.log('Pinging server to keep it alive...');
     
     const options = {
-      hostname: 'fotomate.vercel.app',
-      method: 'GET',
-      timeout: 60000 
+        hostname: 'fotomate.vercel.app',
+        method: 'GET',
+        timeout: 60000 
     };
-  
-    const req = https.request(options, (res) => {
-      console.log(`Ping response: ${res.statusCode}`);
-    });
-  
-    req.on('timeout', () => {
-      req.abort();
-      console.error('Request timed out');
-    });
-  
-    req.on('error', (err) => {
-      console.error('Ping error:', err.message);
-    });
-  
-    req.end();
-});
 
-module.exports = job;
+    const req = https.request(options, (res) => {
+        console.log(`Ping response: ${res.statusCode}`);
+    });
+
+    req.on('timeout', () => {
+        req.destroy();
+        console.error('Request timed out');
+    });
+
+    req.on('error', (err) => {
+        console.error('Ping error:', err.message);
+    });
+
+    req.end();
+}
+
+export const job = cron.schedule('*/14 * * * *', pingServer);
+
+job.start();
--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-import express from 'express';
-import { createServer } from 'http'; 
-import cors from 'cors';
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
 
-import dbconnect from './config/dbConnection.js';
-import userRoute from './routes/userRoute.js';
-import adminRoute from './routes/adminRoute.js';
-import vendorRoute from './routes/vendorRoute.js';
-import chatRouter from './routes/chatRoute.js';
-import setupSocketIO from './socketIo.js';
-import messageRouter from './routes/messageRoute.js';
+import dbconnect from "./config/dbConnection.js";
+import userRoute from "./routes/userRoute.js";
+import adminRoute from "./routes/adminRoute.js";
+import vendorRoute from "./routes/vendorRoute.js";
+import chatRouter from "./routes/chatRoute.js";
+import setupSocketIO from "./socketIo.js";
+import messageRouter from "./routes/messageRoute.js";
+import { job } from "./config/cronJob.js";
 
 const app = express();
-const server = createServer(app); 
+const server = createServer(app);
 
 const PORT = 3000;
-app.use(cors({ 
-  // origin: allowedOrigins,
-  credentials:true
- }));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 dbconnect();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use('/', userRoute); 
-app.use('/admin', adminRoute);
-app.use('/vendor', vendorRoute);
-app.use('/chat',chatRouter)
-app.use('/message',messageRouter)
+app.use("/", userRoute);
+app.use("/admin", adminRoute);
+app.use("/vendor", vendorRoute);
+app.use("/chat", chatRouter);
+app.use("/message", messageRouter);
 
-app.use((err, req, res, next) => { 
+app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 server.listen(PORT, () => {
